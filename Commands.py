069e05e5acf9199ae2a8aa0df8eb6e697f32bed1
Jason Utt <jutt@codedcontainer.com>
2022-09-17 13:59:57 -0400

    print piles

--- Commands.py
+++ Commands.py
@@ -1,8 +1,16 @@
 class Commands:
+  
     @staticmethod
-    def draw(root, cg):
-        cg.draw()
-
+    def print_piles(root, cg):
         root.wp_label["text"]= "Work pile: " +cg.pileMap['work'].print_cards()
         root.fp_label["text"]= "Fun pile: " +cg.pileMap['fun'].print_cards()
         root.dp_label["text"]= "Discard pile: " +cg.pileMap['discard'].print_cards()
+
+    @staticmethod
+    def draw(root, cg):
+        cg.draw()
+        Commands.print_piles(root, cg)
+
+    @staticmethod
+    def discard_pile(root, cg)
+        cg    
